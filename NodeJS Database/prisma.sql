6eadd15880ecedc8803dea1a9543c60e3a385068
“[Fadel <[fadelmuhammad128@gmail.com]>
2023-06-23 09:37:15 +0700

    aggregate

--- NodeJS Database/prisma.sql
+++ NodeJS Database/prisma.sql
@@ -1,4 +1,4 @@
-show databases
+show databases;
 use belajar_nodejs_prisma;
 
 show tables;
@@ -20,6 +20,37 @@ CREATE TABLE customers
 
 select * from customers;
 
+use belajar_nodejs_prisma;
 
+CREATE TABLE products
+(
+    id VARCHAR(100) PRIMARY KEY NOT NULL ,
+    name VARCHAR(100) NOT NULL ,
+    price VARCHAR(100) NOT NULL,
+    stock VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL
+
+) ENGINE INNODB;
+
+select *
+from products;
+
+insert into products(id, name, price, stock, category)
+    values
+    ('P0001', 'A', 1000, 100, 'K1'),
+    ('P0002', 'B', 2000, 200, 'K1'),
+    ('P0003', 'C', 3000, 300, 'K1'),
+    ('P0004', 'D', 4000, 400, 'K1'),
+    ('P0005', 'E', 5000, 500, 'K1');
+
+
+insert into products(id, name, price, stock, category)
+    value ('P0006', 'A', 1000, 100, 'K2'),
+    ('P0007', 'B', 2000, 200, 'K2'),
+    ('P0008', 'C', 3000, 300, 'K2'),
+    ('P0009', 'D', 4000, 400, 'K2'),
+    ('P0010', 'E', 5000, 500, 'K2');
+select *
+from products;
 
 
